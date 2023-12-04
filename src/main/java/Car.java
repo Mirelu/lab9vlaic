918879a412f03ba0f76a68fe9fbeaa466bd19e75
Vlaic C.I. Mirel-Ioan <cti22c114@cst.uoradea.local>
2023-12-04 18:55:33 +0200

    Initial commit

---
+++ src/main/java/Car.java
@@ -0,0 +1,12 @@
+public class Car {
+
+    private String color;
+    private String type;
+    public Car(String color,String type){
+    this.color=color;
+    this.type=type;
+}
+    public static void main(String[] args) {
+
+    }
+}
